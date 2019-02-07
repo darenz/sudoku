6ecef6f810571b9159326098533ec54f59d68550
manjaro <zhujianxyz@163.com>
2019-02-07 22:17:36 +0800

    a new generation and solution

--- sudoku.cpp
+++ sudoku.cpp
@@ -23,10 +23,11 @@ class sudoku
         void draw();
         void play();
         bool judge(int row,int col);
+        bool solve(int row,int col);
+        void getValid(int &row,int &col);
         bool isInBoundary(int x,int y);
         int getScrX(int x);
         int getScrY(int y);
-        void setColor(int COLOR);
 
         int table[9][9];
         int init_tatal_numbers;
@@ -42,12 +43,6 @@ sudoku::sudoku()
 {
 
 }
-
-//sudoku::sudoku(int total_min,int total_max)
-//{
-//    TOTAL_MIN = total_min;
-//    TOTAL_MAX = total_max;
-//}
 
 void sudoku::run()
 {
@@ -104,44 +99,75 @@ void sudoku::run()
         generate();
         draw();
         play();
-
-    }
+   }
 }
 
 void sudoku::generate()
-{
+{   
+    int table_t[9][9] = 
+    {
+     {8,0,0,0,0,0,0,0,0},
+     {0,0,3,6,0,0,0,0,0},
+     {0,7,0,0,9,0,2,0,0},
+     {0,5,0,0,0,7,0,0,0},
+     {0,0,0,8,4,5,7,0,0},
+     {0,0,0,1,0,0,0,3,0},
+     {0,0,1,0,0,0,0,6,8},
+     {0,0,8,5,0,0,0,1,0},
+     {0,9,0,0,0,0,4,0,0}
+    };
+    memcpy(table,table_t,sizeof(table));
+    solve(0,0);
+
     static default_random_engine e;
     e.seed(time(0));
     static uniform_int_distribution<unsigned> 
         total_numbers(TOTAL_MIN,TOTAL_MAX),
         get_number(1,9),
+        get_shuffle_times(0,10),
         get_position(0,8);
-    memset(table,0,sizeof(table));
+
     memset(official_table,0,sizeof(official_table));
     init_tatal_numbers = total_numbers(e);
-    for(int i=0;i<init_tatal_numbers;)
+
+    int shuffle_times = get_shuffle_times(e);
+    int shuffle_table[9] = {1,2,3,4,5,6,7,8,9};
+    for(int i=0;i<shuffle_times;)
     {
-        int row = get_position(e),col = get_position(e);
-        if(table[row][col] !=0 )
+        int srs_pos = get_position(e);
+        int dst_pos = get_position(e);
+        if(srs_pos==dst_pos)
             continue;
         else
         {
-            table[row][col] = get_number(e);
-            if(judge(row,col)==true)
-            {
-                official_table[row][col] = true;
-                i++;
-                continue;
-            }
-            else
-                table[row][col] = 0;
+            int t = shuffle_table[srs_pos];
+            shuffle_table[srs_pos] = shuffle_table[dst_pos];
+            shuffle_table[dst_pos] = t;
+            i++;
+        }
+    }
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            table[i][j] = shuffle_table[table[i][j]-1];
+        }
+    }
+    for(int i=0;i<81-init_tatal_numbers;)
+    {
+        int row = get_position(e);
+        int col = get_position(e);
+        if(table[row][col] == 0)
+            continue;
+        else
+        {
+            table[row][col] = 0;
+            official_table[row][col] = 1;
+            i++;
         }
     }
 }
 
-// -------------------------------------
-// | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
-//
 void sudoku::draw()
 {
     clear();
@@ -236,7 +262,7 @@ void sudoku::draw()
     }
     string s1 = "VIM STYLE -- Up:k Down:j Left:h Right:l";
     string s2 = "NORMAL STYLE -- Up:w Down:s Left:a Right:d";
-    string s3 = "Quit:q Restart:r";
+    string s3 = "Quit:q Restart:r Answer:z";
     mvaddstr(draw_table_y_end+1,(getmaxx(stdscr) - s1.size())/2,s1.c_str());
     mvaddstr(draw_table_y_end+2,(getmaxx(stdscr) - s2.size())/2,s2.c_str());
     mvaddstr(draw_table_y_end+3,(getmaxx(stdscr) - s3.size())/2,s3.c_str());
@@ -295,9 +321,15 @@ void sudoku::play()
             endwin();
             exit(0);
         }
+        else if(key == 'z' || key =='Z')
+        {
+            clear();
+            solve(0,0);
+            draw();
+        }
         else if( key>='1' && key<='9' || key==' ')//空格键消除        
         {//生成的数不允许覆盖
-            if(official_table[table_row][table_col]==true)
+            if(official_table[table_row][table_col]==0)
                 continue;
             else
             {
@@ -314,6 +346,42 @@ void sudoku::play()
         if(to_fill == 0)
             return;
     }
+}
+
+void sudoku::getValid(int &row,int &col)
+{
+    while(col<=9 && row<9 )
+    {
+        if(col==9)
+        {
+            row++;
+            col=0;
+        }
+        if(row==9)
+            return;
+        if(table[row][col]!=0)
+            col++;
+        else
+            break;
+    }
+}
+
+bool sudoku::solve(int row,int col)
+{
+    getValid(row,col);
+    if(row==9)
+        return true;
+    for(int i=1;i<=9;i++)
+    {
+        table[row][col] = i;
+        if(judge(row,col))
+        {
+            if(solve(row,col+1))
+                return true;
+        }
+    }
+    table[row][col] = 0;
+    return false;
 }
 
 bool sudoku::judge(int row,int col)
